9a91a292f403ec1c68335d1e2d1cf922f26f8ccd
Muhammad Noor Alvan Octavian Anwar <alvan.ghon@gmail.com>
2020-01-01 23:57:13 +0700

    Memasukkan Akademik SD

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 let biodata = {
     nama : "Moch Noor Alvan Octavian Anwar",
+    naPang : "Alvan",
     umur : 23,
     Alamat : {
-        Jalan : "Abu Ali 29",
+        Jalan : "Ada Ada Aja 10",
         Kota : "Malang",
         Provinsi : "Jawa Timur"
-    }
-    skills : ['PHP', 'JS', 'CSS3']
+    },
+    Akademi :{
+        SD : {
+            NamaSekolah : "SDN Girimoyo 01",
+            Alamat : {
+                Jalan : "Diponegoro No. 68",
+                Kota : "Malang",
+                Provinsi : "Jawa Timur"
+            },
+            Tahun :{
+                Masuk : "2002/2003",
+                Keluar : "2003/2009"
+            }
+        },
+        SMP : {
+            NamaSekolah : "SDN Girimoyo 01",
+            Alamat : {
+                Jalan : "Diponegoro No. 68",
+                Kota : "Malang",
+                Provinsi : "Jawa Timur"
+            },
+            Tahun :{
+                Masuk : "2002/2003",
+                Keluar : "2003/2009"
+            }
+        }
+    },
+    skills : ['PHP', 'JS', 'CSS3', 'HTML5']
 }
 
-console.log(biodata);
+console.log(biodata.Akademi.SD);
