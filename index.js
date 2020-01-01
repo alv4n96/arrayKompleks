6eadf88abb2892e6ac4b682d649357d69fe83680
Muhammad Noor Alvan Octavian Anwar <alvan.ghon@gmail.com>
2020-01-02 00:28:43 +0700

    Memasukkan Model Penulisan JSON

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ let biodata = {
         Kota : "Malang",
         Provinsi : "Jawa Timur"
     },
+    SosialMedia :[
+        {
+            Deskripsi : "Gihub",
+            URL : "www.github.com/alv4n96"
+        },{
+            Deskripsi: "twitter",
+            URL: "www.twitter.com/alv4n96"
+        },{
+            Deskripsi: "Facebook",
+            URL: "www.facebook.com/alv4n96"
+        }, {
+            Deskripsi: "Instagram",
+            URL: "www.instagram.com/alv4n96"
+        },
+    ],
     Akademi :{
         SD : {
             NamaSekolah : "SD Negeri Girimoyo 01 Karangploso",
@@ -68,4 +83,96 @@ let biodata = {
     skills : ['PHP', 'JS', 'CSS3', 'HTML5']
 }
 
+let biodataJson = {
+    "nama": "Moch Noor Alvan Octavian Anwar",
+    "naPang": "Alvan",
+    "umur": 23,
+    "Alamat": {
+        "Jalan": "Ada Ada Aja No. 10",
+        "Kecamatan": "Karangploso",
+        "Kota": "Malang",
+        "Provinsi": "Jawa Timur"
+    },
+    "SosialMedia": [{
+            "Deskripsi": "Gihub",
+            "URL": "www.github.com/alv4n96"
+        },
+        {
+            "Deskripsi": "twitter",
+            "URL": "www.twitter.com/alv4n96"
+        },
+        {
+            "Deskripsi": "Facebook",
+            "URL": "www.facebook.com/alv4n96"
+        },
+        {
+            "Deskripsi": "Instagram",
+            "URL": "www.instagram.com/alv4n96"
+        }
+    ],
+    "Akademi": {
+        "SD": {
+            "NamaSekolah": "SD Negeri Girimoyo 01 Karangploso",
+            "Alamat": {
+                "Jalan": "Diponegoro No. 68",
+                "Kecamatan": "Karangploso",
+                "Kota": "Malang",
+                "Provinsi": "Jawa Timur"
+            },
+            "Tahun": {
+                "Masuk": "2002/2003",
+                "Keluar": "2003/2009"
+            }
+        },
+        "SMP": {
+            "NamaSekolah": "Smp Islam Al ma'arif 01 singosari",
+            "Alamat": {
+                "Jalan": "Ronggolawe No.19",
+                "Kecamatan": "Singosari",
+                "Kota": "Malang",
+                "Provinsi": "Jawa Timur"
+            },
+            "Tahun": {
+                "Masuk": "2009/2010",
+                "Keluar": "2012/2013"
+            }
+        },
+        "SMK": {
+            "NamaSekolah": "SMK Negeri 2 Singosari",
+            "Alamat": {
+                "Jalan": "Perusahaan No.20",
+                "Kecamatan": "Singosari",
+                "Kota": "Malang",
+                "Provinsi": "Jawa Timur"
+            },
+            "Jurusan": "Multimedia",
+            "Tahun": {
+                "Masuk": "2012/2013",
+                "Keluar": "2013/2015"
+            }
+        },
+        "KULIAH": {
+            "NamaSekolah": "Universitas Negeri Malang",
+            "Alamat": {
+                "Jalan": "Semarang No.5",
+                "Kecamatan": "Lowokwaru",
+                "Kota": "Malang",
+                "Provinsi": "Jawa Timur"
+            },
+            "Jurusan": "S1 Pendidikan Teknik Informatika",
+            "Tahun": {
+                "Masuk": "2015",
+                "Keluar": "2019"
+            }
+        }
+    },
+    "skills": [
+        "PHP",
+        "JS",
+        "CSS3",
+        "HTML5"
+    ]
+}
+
 console.log(biodata);
+console.log(biodataJson);
