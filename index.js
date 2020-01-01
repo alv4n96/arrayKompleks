a0064f7dca46dc7bac97ea382a43a46c62509a30
Muhammad Noor Alvan Octavian Anwar <alvan.ghon@gmail.com>
2020-01-01 23:33:39 +0700

    Memasukkan Alamat

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let biodata = {
-    name : "Moch Noor Alvan",
-    skills : ['PHP', 'JS', 'CSS3'],
-    description : function(){
-        return "nama saya "+ this.name;
+    nama : "Moch Noor Alvan Octavian Anwar",
+    umur : 23,
+    Alamat : {
+        Jalan : "Abu Ali 29",
+        Kota : "Malang",
+        Provinsi : "Jawa Timur"
     }
+    skills : ['PHP', 'JS', 'CSS3']
 }
 
-console.log(biodata.description());
+console.log(biodata);
